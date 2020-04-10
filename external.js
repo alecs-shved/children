3184972a63811b23ae3e0908ce8e2e29f9b9e09f
alecs shved <shveduk-al@mail.ru>
2020-04-10 11:54:57 +0300

    10.04.20  11.54

--- external.js
+++ external.js
@@ -1,14 +1,165 @@
-let ff = "shved";
-console.log(ff);
+//let ff = "shved";
+//console.log(ff);
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-     max = Math.floor(max);
-     return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+//function getRandomInt(min, max) {
+  //  min = Math.ceil(min);
+  //   max = Math.floor(max);
+  //   return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+//}
+//console.log(getRandomInt(1, 100));
+function alfavv() {
+    location.href = "indexalfav.html";
+    } 
+    function pishem() {
+        location.href = "index.html";
+        } 
+        function slogi() {
+            location.href = "indexslogi.html";
+            } 
+            function chit() {
+                location.href = "indexchit.html";
+                } 
+                function schit() {
+                    location.href = "indexschit.html";
+                    } 
+
+var imagchit0 = ["image0/gora.jpeg", "image0/kasha.jpeg", "image0/mama.jpeg", "image0/masha.jpeg", "image0/mir.jpeg", "image0/nos.jpeg", "image0/papa.jpeg",
+    "image0/popa.jpeg", "image0/rot.jpeg", "image0/sok.jpeg"
+],
+    imagchit1 = ["image1/bik.jpeg", "image1/chees.jpeg", "image1/dom.jpeg", "image1/ejik.jpeg", "image1/juk.jpeg", "image1/kit.jpeg", "image1/kot.jpeg",
+        "image1/luk.jpeg", "image1/rak.jpeg", "image1/ula.jpeg"
+    ],
+    imagchit2 = ["image2/chasi.jpeg", "image2/chuka.jpeg", "image2/lisa.jpeg", "image2/morj.jpeg", "image2/pauk.jpeg", "image2/slon.jpeg", "image2/sova.jpeg",
+        "image2/tort.jpeg", "image2/yaico.jpeg", "image2/ytka.jpeg"
+    ],
+    imagchit3 = ["image3/belka.jpeg", "image3/doctor.jpeg", "image3/fixk.jpeg", "image3/induk.jpeg", "image3/mishka.jpeg", "image3/pavlin.jpeg", "image3/petuh.jpeg",
+        "image3/simka.png", "image3/sobaka.jpeg", "image3/zebra.jpeg"
+    ],
+    imamchit = imagchit1;
+
+//document.getElementById('myImg1').src = imag1[0];
+document.getElementById('myImgchit2').src = imagchit1[0];
+document.getElementById('myImgchit3').src = imagchit1[1];
+document.getElementById('myImgchit4').src = imagchit1[2];
+document.getElementById('myImgchit5').src = imagchit1[3];
+document.getElementById('myImgchit6').src = imagchit1[4];
+document.getElementById('myImgchit7').src = imagchit1[5];
+document.getElementById('myImgchit8').src = imagchit1[6];
+document.getElementById('myImgchit9').src = imagchit1[7];
+document.getElementById('myImgchit10').src = imagchit1[8];
+document.getElementById('myImgchit11').src = imagchit1[9];
+
+function myher111(par1, par2, ii) {
+    //clx();
+    //cly();
+   // per = ii;
+   // document.getElementById('myImgchit1').src = par1;
+   // wa = wavm[ii];
+   // world = worm[ii];
+   // document.getElementById('audio1').src = par2;
+   // clx();
+   // cly();
+   // fun1();
+    //document.getElementById('audio1').load();
+   // document.getElementById('audio1').play();
 }
-console.log(getRandomInt(1, 100));
-console.log(getRandomInt(1, 100));
-console.log(getRandomInt(1, 100));
-console.log(getRandomInt(1, 100));
-console.log(getRandomInt(1, 100));
-
+function levelchit1() {
+    document.getElementById('bbchit0').style.background = "#4CAF50";
+    document.getElementById('bbchit1').style.background = "red";
+    document.getElementById('bbchit2').style.background = "#4CAF50";
+    document.getElementById('bbchit3').style.background = "#4CAF50";
+  //  worm = world1;
+  //  world = worm[0];
+   // imam = imag1;
+   // wavm = wava1;
+  //  per = 0;
+   // clx();
+   // cly();
+   // fun1();
+  //  document.getElementById('myImg1').src = imag1[0];
+    document.getElementById('myImgchit2').src = imagchit1[0];
+    document.getElementById('myImgchit3').src = imagchit1[1];
+    document.getElementById('myImgchit4').src = imagchit1[2];
+    document.getElementById('myImgchit5').src = imagchit1[3];
+    document.getElementById('myImgchit6').src = imagchit1[4];
+    document.getElementById('myImgchit7').src = imagchit1[5];
+    document.getElementById('myImgchit8').src = imagchit1[6];
+    document.getElementById('myImgchit9').src = imagchit1[7];
+    document.getElementById('myImgchit10').src = imagchit1[8];
+    document.getElementById('myImgchit11').src = imagchit1[9];
+};
+function levelchit0() {
+     document.getElementById('bbchit0').style.background = "red";
+     document.getElementById('bbchit1').style.background = "#4CAF50";
+     document.getElementById('bbchit2').style.background = "#4CAF50";
+     document.getElementById('bbchit3').style.background = "#4CAF50";
+   //  worm = world1;
+   //  world = worm[0];
+    // imam = imag1;
+    // wavm = wava1;
+   //  per = 0;
+    // clx();
+    // cly();
+    // fun1();
+   //  document.getElementById('myImg1').src = imag1[0];
+     document.getElementById('myImgchit2').src = imagchit0[0];
+     document.getElementById('myImgchit3').src = imagchit0[1];
+     document.getElementById('myImgchit4').src = imagchit0[2];
+     document.getElementById('myImgchit5').src = imagchit0[3];
+     document.getElementById('myImgchit6').src = imagchit0[4];
+     document.getElementById('myImgchit7').src = imagchit0[5];
+     document.getElementById('myImgchit8').src = imagchit0[6];
+     document.getElementById('myImgchit9').src = imagchit0[7];
+     document.getElementById('myImgchit10').src = imagchit0[8];
+     document.getElementById('myImgchit11').src = imagchit0[9];
+ };
+ function levelchit2() {
+    document.getElementById('bbchit0').style.background = "#4CAF50";
+    document.getElementById('bbchit1').style.background = "#4CAF50";
+    document.getElementById('bbchit2').style.background = "red";
+    document.getElementById('bbchit3').style.background = "#4CAF50";
+  //  worm = world1;
+  //  world = worm[0];
+   // imam = imag1;
+   // wavm = wava1;
+  //  per = 0;
+   // clx();
+   // cly();
+   // fun1();
+  //  document.getElementById('myImg1').src = imag1[0];
+    document.getElementById('myImgchit2').src = imagchit2[0];
+    document.getElementById('myImgchit3').src = imagchit2[1];
+    document.getElementById('myImgchit4').src = imagchit2[2];
+    document.getElementById('myImgchit5').src = imagchit2[3];
+    document.getElementById('myImgchit6').src = imagchit2[4];
+    document.getElementById('myImgchit7').src = imagchit2[5];
+    document.getElementById('myImgchit8').src = imagchit2[6];
+    document.getElementById('myImgchit9').src = imagchit2[7];
+    document.getElementById('myImgchit10').src = imagchit2[8];
+    document.getElementById('myImgchit11').src = imagchit2[9];
+};
+function levelchit3() {
+    document.getElementById('bbchit0').style.background = "#4CAF50";
+    document.getElementById('bbchit1').style.background = "#4CAF50";
+    document.getElementById('bbchit2').style.background = "#4CAF50";
+    document.getElementById('bbchit3').style.background = "red";
+  //  worm = world1;
+  //  world = worm[0];
+   // imam = imag1;
+   // wavm = wava1;
+  //  per = 0;
+   // clx();
+   // cly();
+   // fun1();
+  //  document.getElementById('myImg1').src = imag1[0];
+    document.getElementById('myImgchit2').src = imagchit3[0];
+    document.getElementById('myImgchit3').src = imagchit3[1];
+    document.getElementById('myImgchit4').src = imagchit3[2];
+    document.getElementById('myImgchit5').src = imagchit3[3];
+    document.getElementById('myImgchit6').src = imagchit3[4];
+    document.getElementById('myImgchit7').src = imagchit3[5];
+    document.getElementById('myImgchit8').src = imagchit3[6];
+    document.getElementById('myImgchit9').src = imagchit3[7];
+    document.getElementById('myImgchit10').src = imagchit3[8];
+    document.getElementById('myImgchit11').src = imagchit3[9];
+};
